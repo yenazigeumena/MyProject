0c1820d19ee847d5599e52f5e004af2f97b50253
yenazigeumena <gogguu@bu.ac.kr>
2024-11-21 21:36:54 +0900

    포인트 충전 페이지로 이동_수정

--- app/src/main/java/com/example/myproject/Personal.java
+++ app/src/main/java/com/example/myproject/Personal.java
@@ -1,12 +1,28 @@
 package com.example.myproject;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Personal extends MainActivity{
 
+    Button btnCard;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personal);
+        btnCard = (Button) findViewById(R.id.card_charge);
+
+        btnCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CardCharge.class);
+                startActivity(intent);
+            }
+        });
     }
 }
