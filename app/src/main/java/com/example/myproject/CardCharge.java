2388c44a09065ec443c1bd91784e205976a982ff
yenazigeumena <gogguu@bu.ac.kr>
2024-11-21 21:36:18 +0900

    포인트 충전 페이지_초안

---
+++ app/src/main/java/com/example/myproject/CardCharge.java
@@ -0,0 +1,17 @@
+package com.example.myproject;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class CardCharge extends Personal{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.card_charge);
+
+
+
+    }
+}
